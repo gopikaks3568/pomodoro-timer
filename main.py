6e275d0507f9a25fa20c24ada8ba64cd6c5c5a5a
gopika <gopikaks3568@gmail.com>
2026-06-24 19:45:17 +0530

    Add a count down mechanism and setting sessions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -5,15 +6,67 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
+reps = 1
+new_text = "✔"
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+
+    if reps % 2 != 0:
+        global new_text
+        timer_label.config(text="Study", fg=PINK )
+        tick_counter_label.config(text=new_text)
+        tick_counter_label.grid(row=3, column=1)
+        count_down(work_sec)
+
+    elif reps % 8 == 0:
+        timer_label.config(text="Long Break", fg =RED )
+        count_down(long_break_sec)
+    else:
+        timer_label.config(text="Break", fg =GREEN )
+        count_down(short_break_sec)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    global reps
+    global new_text
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down,count -1)
+
+
+
+    if count == 0 :
+        reps += 1
+        text = "✔"
+        session = 1
+
+        if reps % 2 != 0:
+            new_text += text
+
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,10 +79,10 @@ timer_label.grid(row=0, column=1)
 canvas = Canvas(height=224, width=200, bg= YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100,130 ,text="00:00",fill= "white",font=(FONT_NAME,25,"bold"))
+timer_text = canvas.create_text(100,130 ,text="00:00",fill= "white",font=(FONT_NAME,25,"bold"))
 canvas.grid(row=1, column =1)
 
-start_button = Button(text= "Start")
+start_button = Button(text= "Start", command=start_timer)
 start_button.config(font=FONT_NAME)
 start_button.grid(row=3, column=0)
 
