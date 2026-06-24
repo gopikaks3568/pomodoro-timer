aed6fe8531d4cf80ae814b4ef892b71a04028466
gopika <gopikaks3568@gmail.com>
2026-06-24 15:23:34 +0530

    App UI

--- main.py
+++ main.py
@@ -20,12 +20,26 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg= YELLOW)
 
+timer_label  = Label(text="Timer", font=(FONT_NAME, 30, "bold"), fg=GREEN, bg= YELLOW)
+timer_label.grid(row=0, column=1)
 
 canvas = Canvas(height=224, width=200, bg= YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100,130 ,text="00:00",fill= "white",font=(FONT_NAME,25,"bold"))
-canvas.pack()
+canvas.grid(row=1, column =1)
+
+start_button = Button(text= "Start")
+start_button.config(font=FONT_NAME)
+start_button.grid(row=3, column=0)
+
+reset_button = Button(text= "Reset")
+reset_button.config(font=FONT_NAME)
+reset_button.grid(row=3, column=3)
+
+tick_counter_label = Label(text="✔",fg=GREEN, font=(FONT_NAME, 20, "bold"), bg=YELLOW,pady=30)
+tick_counter_label.grid(row=3, column=1)
+
 
 
 
